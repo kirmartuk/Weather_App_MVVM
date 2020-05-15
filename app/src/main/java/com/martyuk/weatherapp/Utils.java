4f9715fb1f37173cafff3b70dd1a3ddcd84f9c33
kirmartuk <kirillmartuk@gmail.com>
2020-05-15 20:38:21 +0700

    add getSystemLanguage function

--- app/src/main/java/com/martyuk/weatherapp/Utils.java
+++ app/src/main/java/com/martyuk/weatherapp/Utils.java
@@ -1,9 +1,30 @@
 package com.martyuk.weatherapp;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.util.Log;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Utils {
     public static int getImageRes(Context context, String imageName) {
         return context.getResources().getIdentifier(imageName, "drawable", context.getPackageName());
+    }
+
+    public static Retrofit getRetrofit(String baseUrl) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        return retrofit;
+    }
+    public static String getSystemLanguage(){
+        String locale = Resources.getSystem().getConfiguration().locale.getLanguage();
+        switch (locale){
+            case "cs":
+                return "cz";
+        }
+        return locale;
     }
 }
