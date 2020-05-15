7876a4fcd3f8161b422a37ab9a2850cce2228b01
kirmartuk <kirillmartuk@gmail.com>
2020-05-15 21:23:03 +0700

    add documentation for function && update system language function

--- app/src/main/java/com/martyuk/weatherapp/Utils.java
+++ app/src/main/java/com/martyuk/weatherapp/Utils.java
@@ -2,16 +2,24 @@ package com.martyuk.weatherapp;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.util.Log;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class Utils {
+    /**
+     * @param context
+     * @param imageName
+     * @return int image resource for openweathermap data
+     */
     public static int getImageRes(Context context, String imageName) {
         return context.getResources().getIdentifier(imageName, "drawable", context.getPackageName());
     }
 
+    /**
+     * @param baseUrl
+     * @return retrofit by base url
+     */
     public static Retrofit getRetrofit(String baseUrl) {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(baseUrl)
@@ -19,11 +27,31 @@ public class Utils {
                 .build();
         return retrofit;
     }
-    public static String getSystemLanguage(){
+
+    /**
+     * some languages differ in Android os and API
+     *
+     * @return system language for openWeatherMap.org
+     */
+    public static String getSystemLanguage() {
         String locale = Resources.getSystem().getConfiguration().locale.getLanguage();
-        switch (locale){
+        switch (locale) {
             case "cs":
                 return "cz";
+            case "sq":
+                return "al";
+            case "iw":
+                return "he";
+            case "in":
+                return "id";
+            case "ko":
+                return "kr";
+            case "lv":
+                return "la";
+            case "nb":
+                return "no";
+            case "zn":
+                return "zn_ch";
         }
         return locale;
     }
